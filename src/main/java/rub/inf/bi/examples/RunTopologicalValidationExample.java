353ca025afbc49418b66d1168c1568d3c5840348
Yifeng He <mellome1992@gmail.com>
2022-10-13 19:47:10 +0200

    add xml generator

--- src/main/java/rub/inf/bi/examples/RunTopologicalValidationExample.java
+++ src/main/java/rub/inf/bi/examples/RunTopologicalValidationExample.java
@@ -1,10 +1,36 @@
 package rub.inf.bi.examples;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+ 
+import java.io.StringReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.apache.jena.geosparql.configuration.GeoSPARQLConfig;
 import org.apache.jena.geosparql.implementation.index.IndexConfiguration.IndexOption;
 import org.apache.jena.query.ParameterizedSparqlString;
@@ -13,8 +39,11 @@ import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.RDFWriter;
 import org.apache.jena.riot.RDFDataMgr;
 import rub.inf.bi.extension.jena.ExtendedFunctionConfig;
 import rub.inf.bi.extension.jena.NamespaceManager;
@@ -24,6 +53,7 @@ public class RunTopologicalValidationExample {
     // define directories
     private static String ontologiePath = "/Users/yhe/Developer/Repo/ExtendingJena/src/main/resources/rdf/3DScenario.rdf";
     private static String originalQueryPath = "/Users/yhe/Developer/Repo/ExtendingJena/src/main/resources/sparql/Geometry3D Tests/Test3_TopologicalWithin.ttl";
+    private static String queryResultPath = "/Users/yhe/Developer/Repo/ExtendingJena/src/main/resources/rdf/query_result.xml";
 
     public static void main(String[] args) {
         //Definition of all namespaces in use
@@ -81,15 +111,63 @@ public class RunTopologicalValidationExample {
         System.out.println("\n==================Result===================");
 		try(QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
 	        ResultSet results = qexec.execSelect() ;
-	        for ( ; results.hasNext() ; )
-	        {
-	            QuerySolution sol = results.nextSolution() ;
-                System.out.println(sol.getResource("GeometryA") + " | " + sol.getResource("GeometryB") );
-	            // RDFNode x = sol.get("f") ;
-	            // System.out.println(x);
-	        }
+            Boolean isColumnNameStored = false;
+            List<String> columnNames = new ArrayList<>();
+
+            //save query results to a .xml file
+            String xmlString = ResultSetFormatter.asXMLString(results);
+            System.out.println(xmlString);
+            try {
+                stringToDom(xmlString, queryResultPath);
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+
+            // RDFWriter rdfWriter = model.getWriter("RDF/XML");
+            // rdfWriter.write(model, new FileOutputStream("/Users/yhe/Developer/Repo/ExtendingJena/src/main/resources/rdf/queried_3DScenario.xml"), xmlString);
+            
+            // print query results
+	        // for ( ; results.hasNext() ; )
+	        // {
+	        //     QuerySolution sol = results.nextSolution() ;
+            //     if(isColumnNameStored == false){
+            //         // this progress runs only once within the for-loop
+            //         sol.varNames().forEachRemaining(columnNames::add);
+            //         isColumnNameStored = true;
+            //     }
+            //     // System.out.println(sol.getResource(columnNames.get(0)) + " | " + sol.getResource(columnNames.get(1)) );
+
+            //     List<RDFNode> nodeList = new ArrayList<>();
+            //     for(String var : columnNames){
+            //         nodeList.add(sol.get(var));
+            //     }
+
+            //     System.out.println(Arrays.toString(nodeList.toArray()));
+	        // }
 	    }
 		System.out.println("===========================================");
 
+    }
+
+    private static void stringToDom(String xmlString, String pathName) throws SAXException, ParserConfigurationException, IOException {
+    // Parse the given input
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    org.w3c.dom.Document doc = builder.parse(new InputSource(new StringReader(xmlString)));
+
+    // Write the parsed document to an xml file
+    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    try {
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result =  new StreamResult(new File(pathName));
+        try {
+            transformer.transform(source, result);
+        } catch (TransformerException e) {
+            e.getMessage();
+        }
+    } catch (TransformerConfigurationException e) {
+        e.getStackTrace();
+    }
     }
 }
